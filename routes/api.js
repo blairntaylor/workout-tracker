2137b53fdef0fd7dc3d1157c7f8f4ad8127913b5
Blair Taylor <blairntaylor@gmail.com>
2021-03-15 08:43:16 -0600

    complete put route

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ const db = require("../models");
 
 module.exports = (app) => {
   //app.get for find
-  app.get("/api/workout", (req, res) => {
+  app.get("/api/workouts", (req, res) => {
     db.Workout.find({}, (err, workouts) => {
       if (err) {
         console.log(err);
@@ -12,7 +12,7 @@ module.exports = (app) => {
     });
   });
   //app.post to make new workout
-  app.post("/api/workout", (req, res) => {
+  app.post("/api/workouts", (req, res) => {
     db.Workout.create({}, (err, workouts) => {
       if (err) {
         console.log(err);
@@ -44,13 +44,17 @@ module.exports = (app) => {
   });
 
   //app to add exercise to workout
-  app.put("/api/workout/:workout", ({ body }, res) => {
+  app.put("/api/workouts/:workout", ({ body, params }, res) => {
     db.Workout.findByIdAndUpdate(
-      {},
+      { _id: params.workout },
       { $push: { exercises: body } },
       { new: true, useFindAndModify: false }
-    ).then((Workout) => {
-      res.json(Workout);
-    });
+    )
+      .then((Workout) => {
+        res.json(Workout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
   });
 };
